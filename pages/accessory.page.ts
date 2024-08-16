2a1428ebc4f58c4a7b05c1b9daf29da6dd389a30
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-08-16 17:23:22 +0200

    add accessory page

---
+++ pages/accessory.page.ts
@@ -0,0 +1,34 @@
+import { Locator, Page } from '@playwright/test'
+
+export class AccessoryPage {
+  readonly page: Page
+
+  readonly productAddedMessage: Locator
+  readonly cartCount: Locator
+  readonly thorHammer: Locator
+  readonly saw: Locator
+  readonly logoToolShop: Locator
+  readonly cartIcon: Locator
+  readonly productName: Locator
+  readonly proceedButton: Locator
+  readonly messageYouReLogged: Locator
+  readonly successButton: Locator
+  readonly pageTitle: Locator
+
+  constructor(page: Page) {
+    this.page = page
+
+    this.productAddedMessage = page.getByLabel('Product added to shopping')
+    this.cartCount = page.locator('#lblCartCount')
+    this.thorHammer = page.locator('//h5[text()=" Thor Hammer "]')
+    this.saw = page.locator('//h5[text()=" Circular Saw "]')
+    this.cartIcon = page.locator('//a[@class="nav-link" and @href="/checkout"]')
+    this.logoToolShop = page.locator('#Layer_1')
+    this.productName = page.locator('.product-title')
+    this.proceedButton = page.locator('[data-test="proceed-2"]')
+    this.messageYouReLogged = page.getByText('Hello Jack Howe, you are')
+    this.successButton = page.locator("//button[text()='Proceed to checkout']")
+
+    this.pageTitle = page.locator('[data-test="page-title"]')
+  }
+}
--- tests/buyhammer.spec.ts
+++ tests/buyhammer.spec.ts
@@ -2,89 +2,82 @@ import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login.page'
 import { ToolsPage } from '../pages/tools.page'
 import { DeliveryPage, billingAddressModel } from '../pages/delivery.page'
+import { AccessoryPage } from '../pages/accessory.page'
 
-test.describe.only('end to end tests', () => {
+test.describe.parallel('end to end tests', () => {
   let loginPage: LoginPage
   let toolsPage: ToolsPage
   let deliveryPage: DeliveryPage
+  let accessoryPage: AccessoryPage
   test.beforeEach('login test', async ({ page }) => {
     loginPage = new LoginPage(page)
     await loginPage.goToPage(page)
     toolsPage = new ToolsPage(page)
     deliveryPage = new DeliveryPage(page)
+    accessoryPage = new AccessoryPage(page)
   })
   test('login with correct credentials', async ({ page }) => {
     const dataEmail = 'customer2@practicesoftwaretesting.com'
     const dataPass = 'welcome01'
-    const pageTitle = page.locator('[data-test="page-title"]')
+    // const pageTitle = page.locator('[data-test="page-title"]')
     const textMyAccount = 'My account'
 
     await loginPage.login(dataEmail, dataPass)
     await page.waitForLoadState()
-    await expect(pageTitle).toContainText(textMyAccount)
+    await expect(accessoryPage.pageTitle).toContainText(textMyAccount)
   })
 
   test('first end to end test', async ({ page }) => {
     // Arrange
-    const productAddedMessage = page.getByLabel('Product added to shopping')
-    const cartCount = page.locator('#lblCartCount')
-    const thorHammer = page.locator('//h5[text()=" Thor Hammer "]')
-    const saw = page.locator('//h5[text()=" Circular Saw "]')
-    const logoToolShop = page.locator('#Layer_1')
-    const cartIcon = page.locator(
-      '//a[@class="nav-link" and @href="/checkout"]',
-    )
-    const productName = page.locator('.product-title')
-    const proceedButton = page.locator('//button[text()="Proceed to checkout"]')
     const dataEmail = 'customer2@practicesoftwaretesting.com'
     const dataPass = 'welcome01'
     const emailInput = page.locator('#email')
     const passwordInput = page.locator('#password')
     const submitButton = page.locator('.btnSubmit')
-    const messageYouReLogged = page.getByText('Hello Jack Howe, you are')
-    const successButton = page.locator('.btn-success')
+
     // Act
     // we search for some hammer
     await toolsPage.search('Hammer')
-
     // click classic claw hammer
-    await toolsPage.chooseItem(' Claw Hammer ')
+    await toolsPage.chooseItem(' Claw Hammer ') // wywalić do konsta!!!
     // add it to cart
     await toolsPage.addToCart()
+    await page.waitForTimeout(3000) // czekamy 3sec
 
     // Assert
-    await expect(productAddedMessage).toBeVisible()
-    await expect(cartCount).toHaveText('1')
+    await expect(accessoryPage.productAddedMessage).toBeVisible()
+    await expect(accessoryPage.cartCount).toHaveText('1')
 
     // Act
     // click on Thor Hammer in related products
     await toolsPage.chooseItem(' Thor Hammer ')
-    await thorHammer.waitFor()
-    await thorHammer.click()
+    await accessoryPage.thorHammer.waitFor()
+    await accessoryPage.thorHammer.click()
     // add it to cart
     await toolsPage.addToCart()
+    await page.waitForTimeout(3000)
 
     // Assert
-    await expect(productAddedMessage).toBeVisible()
-    await expect(cartCount).toHaveText('2')
+    await expect(accessoryPage.productAddedMessage).toBeVisible()
+    await expect(accessoryPage.cartCount).toHaveText('2')
 
     // Act
     // click on Tool Shop Logo in order to go to main page
-    await logoToolShop.click()
+    await accessoryPage.logoToolShop.click()
     await page.waitForLoadState()
     // in search input let's write "saw"
     await toolsPage.search('Saw')
-    await saw.waitFor()
+    await accessoryPage.saw.waitFor()
     // click on circular saw because it looks cool
-    await saw.click()
+    await accessoryPage.saw.click()
     await toolsPage.addToCart()
 
     // Assert
-    await expect(productAddedMessage).toBeVisible()
-    await expect(cartCount).toHaveText('3')
+    await expect(accessoryPage.productAddedMessage).toBeVisible()
+    await expect(accessoryPage.cartCount).toHaveText('3')
 
     // we go to checkout
-    await cartIcon.click()
+    await accessoryPage.cartIcon.click()
 
     // Assert
     // we check if all items are in the cart
@@ -96,7 +89,8 @@ test.describe.only('end to end tests', () => {
 
     //here should be method checking total price of cart (?)
 
-    await proceedButton.click()
+    // await accessoryPage.proceedButton.click()
+    await accessoryPage.successButton.click()
 
     // we need to sign in
     await emailInput.fill(dataEmail)
@@ -105,13 +99,12 @@ test.describe.only('end to end tests', () => {
     await submitButton.click()
     //? or we don't because it says we are already logged in
 
-  
-    await expect(messageYouReLogged).toBeVisible()
-    await page.locator('//button[text()=" Proceed to checkout "]').click()
-    // await proceedButton.click()
+    await expect(accessoryPage.messageYouReLogged).toBeVisible()
+    // await page.pause()
+    await accessoryPage.proceedButton.click()
 
     // we need to fill delivery formular
-    
+
     const billingAddress: billingAddressModel = {
       address: 'Sezamkowa 3/30',
       city: 'Warsaw',
