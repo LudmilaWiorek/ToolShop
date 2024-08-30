9c44ad83f4c8faf4095e5fe85827ce0ba9c3391a
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-08-30 16:10:29 +0200

    add userdata to positive scenario

--- tests/loginwithpage.spec.ts
+++ tests/loginwithpage.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login.page'
+import * as users from '../loginData/users.json'
 
 test.describe('login section tests', () => {
   let loginPage: LoginPage
@@ -9,19 +10,19 @@ test.describe('login section tests', () => {
   })
   //positive scenarios
   // documentation of test cases this case was titled "valid email with a lowercase letter;
-  
-    test('login with correct credentials', async ({ page }) => {
-      const dataEmail = 'customer2@practicesoftwaretesting.com'
-      const dataPass = 'welcome01'
-      const pageTitle = page.locator('[data-test="page-title"]')
-      const textMyAccount = 'My account'
-  
-      await loginPage.login(dataEmail, dataPass)
-      await page.waitForLoadState()
-      await expect(pageTitle).toContainText(textMyAccount)
-    })
+
+  test('login with correct credentials', async ({ page }) => {
+    const dataEmail = users.userdata[0].email
+    const dataPass = users.userdata[0].password
+    const pageTitle = page.locator('[data-test="page-title"]')
+    const textMyAccount = 'My account'
+
+    await loginPage.login(dataEmail, dataPass)
+    await page.waitForLoadState()
+    await expect(pageTitle).toContainText(textMyAccount)
+  })
   // I changed for "uppercase" due to valid account details provided in Github repository of practicesoftwaretesting
-  // disclaimer - we agreed that this test case is hard "depends", I decided to include it here anyway
+  // we agreed that this test case is strong "depends", I decided to include it here anyway
   test('login with correct credentials - valid email started with a uppercase letter', async ({
     page,
   }) => {
