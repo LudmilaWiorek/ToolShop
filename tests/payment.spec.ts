7683ce3574658ab2a6e4decd711326140f7eafc6
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-10-16 16:00:54 +0200

    add positive payment modules tests

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,38 +1,86 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import * as users from '../loginData/users.json'
 import { LoginPage } from '../pages/login.page'
 import { ApiStore } from '../pages/apiClass.page'
 import { AccessoryPage } from '../pages/accessory.page'
+import { DeliveryPage } from '../pages/delivery.page'
+import { PaymentPage } from '../pages/payment.page'
+import { PaymentModel } from '../models/payment.model'
+import { faker } from '@faker-js/faker/locale/en'
 
-test.describe('testing payment module', async () => {
+test.describe.parallel('testing payment module', async () => {
   let apiClass: ApiStore
   let loginPage: LoginPage
   let accessoryPage: AccessoryPage
+  let deliveryPage: DeliveryPage
+  let paymentPage: PaymentPage
 
-  test('POST - creating a cart and add some products into it', async ({
-    request,
-    page,
-  }) => {
-    apiClass = new ApiStore(request)
-    const dataEmail = users.userdata[0].email
-    const dataPass = users.userdata[0].password
+  test.beforeEach(
+    'POST - creating a cart and add some products into it',
+    async ({ request, page }) => {
+      apiClass = new ApiStore(request)
+      const dataEmail = users.userdata[0].email
+      const dataPass = users.userdata[0].password
 
-    // creating cart
-    const cartId = await apiClass.prepareCart() // save result of function to const!!!
-    const cart = await apiClass.getItemsFromCart(cartId)
+      // creating cart
+      const cartId = await apiClass.prepareCart()
 
-    // open browser
-    loginPage = new LoginPage(page)
-    await loginPage.goToPage()
-    await loginPage.login(dataEmail, dataPass)
+      // open browser
+      loginPage = new LoginPage(page)
+      await loginPage.goToPage()
+      await loginPage.login(dataEmail, dataPass)
 
-    await page.evaluate((cartId) => {
-      sessionStorage['cart_id'] = cartId
-      sessionStorage['cart_quantity'] = 5 // because there are 2 parameters in session storage about cart; number is needed just to have possibility to enter the cart
-    }, cartId)
+      await page.evaluate((cartId) => {
+        sessionStorage['cart_id'] = cartId
+        sessionStorage['cart_quantity'] = 5
+        // because there are 2 parameters in session storage about cart; number is needed just to have possibility to enter the cart
+      }, cartId)
 
-    // open cart
-    accessoryPage = new AccessoryPage(page)
-    await accessoryPage.openCart()
+      // open cart
+      accessoryPage = new AccessoryPage(page)
+      await accessoryPage.openCart()
+      await accessoryPage.confirmCart()
+      // confirm login user
+      await accessoryPage.confirmLoginData()
+
+      deliveryPage = new DeliveryPage(page)
+      // we need to fill delivery form and we overwrite built in data
+      await page.waitForTimeout(2000) // timeout is needed so we can overwrite predefined address
+      await deliveryPage.fillDeliveryForm(deliveryPage.billingAddress)
+      await deliveryPage.confirmAddress()
+
+      paymentPage = new PaymentPage(page)
+      const headerPayment = paymentPage.h3Payment
+      await expect(headerPayment).toBeVisible()
+    },
+  )
+
+  test('testing bank transfer module', async () => {
+    const payment: PaymentModel = {
+      method: 'Bank Transfer',
+    }
+    await paymentPage.fillPaymentForm(paymentPage, payment.method)
+    await paymentPage.confirmPayment()
+  })
+  test('testing cash on delivery module', async () => {
+    const payment: PaymentModel = {
+      method: 'Cash on Delivery',
+    }
+    await paymentPage.fillPaymentForm(paymentPage, payment.method)
+    await paymentPage.confirmPayment()
+  })
+  test('testing buy now pay later module', async () => {
+    const payment: PaymentModel = {
+      method: 'Buy Now Pay Later',
+    }
+    await paymentPage.fillPaymentForm(paymentPage, payment.method)
+    await paymentPage.confirmPayment()
+  })
+  test('testing gift card module', async () => {
+    const payment: PaymentModel = {
+      method: 'Gift Card',
+    }
+    await paymentPage.fillPaymentForm(paymentPage, payment.method)
+    await paymentPage.confirmPayment()
   })
 })
