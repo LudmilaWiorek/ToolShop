81e8e195a3608d1cff50c14ea45cba226918c6b0
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-12-09 17:23:33 +0100

    add interface for registration

---
+++ models/registration.model.ts
@@ -0,0 +1,13 @@
+export interface RegistrationModel {
+  firstNameInput: string
+  lastNameInput: string
+  dateOfBirth: string
+  addressInput: string
+  postCode: string
+  city: string
+  state: string
+  country: string
+  phone: string
+  email: string
+  password: string
+}
