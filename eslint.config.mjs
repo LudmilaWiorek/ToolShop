9a80dca0b5313182470097f2a2fec833c85fa4e5
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-12-09 13:46:54 +0100

    continue configure eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,39 @@
 import pluginJs from '@eslint/js'
+import eslintPluginPlaywright from 'eslint-plugin-playwright'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    ignores: ['package-lock-json', 'playwright-report/**', 'test-results/**'],
-  },
+  { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
   { files: ['**/*.ts'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
     rules: {
-      'no-console': 'warn',
+      'no-console': 'error',
     },
   },
   ...tseslint.configs.recommended,
   {
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'error',
-    }
-  }
+    },
+  },
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: {
+      'playwright/no-nested-step': 'off',
+      'playwright/no-raw-locators': 'off',
+      'playwright/no-wait-for-timeout': 'off',
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
 ]
