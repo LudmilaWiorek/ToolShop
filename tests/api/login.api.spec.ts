d9a495136537cf4f508bd948a35d93680b162820
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-06-24 16:26:10 +0200

    add header to login test

--- tests/api/login.api.spec.ts
+++ tests/api/login.api.spec.ts
@@ -1,21 +1,30 @@
 import * as users from '../../JSONS/users.json'
 import { expect, fixtures as test } from '../../fixtures/fixtures.fixture'
-import { ApiStore } from '../../pages/apiClass.page'
+import { ApiClass } from '../../pages/apiClass.page'
 
 test.describe('testing login module', () => {
-  let apiClass: ApiStore
-  test('api login user with correct credentials', async ({ request }) => {
-    apiClass = new ApiStore(request)
-    const response = await request.post(`${apiClass.baseUrl}/users/login`, {
-      data: {
-        email: users.userdata.user[0].email,
-        password: users.userdata.user[0].password,
-      },
-    })
-    const responseBody = JSON.parse(await response.text())
+  let apiClass: ApiClass
+  //   let header: { Authorization: string }
+  test.beforeEach(
+    'api login user with correct credentials',
+    async ({ request }) => {
+      apiClass = new ApiClass(request)
+      const response = await request.post(`${apiClass.baseUrl}/users/login`, {
+        data: {
+          email: users.userdata.user[0].email,
+          password: users.userdata.user[0].password,
+        },
+      })
+      const responseBody = JSON.parse(await response.text())
+      //   console.log(responseBody)
 
-    expect(responseBody).toHaveProperty('access_token')
-    expect(responseBody.access_token).toBeTruthy()
-    expect(response.status()).toBe(200)
-  })
+      expect(responseBody).toHaveProperty('access_token')
+      expect(responseBody.access_token).toBeTruthy()
+      expect(response.status()).toBe(200)
+
+      let header = {
+        Authorization: `Bearer ${responseBody.access_token}`,
+      }
+    },
+  )
 })
