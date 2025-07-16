6f2601f4657b8bd33f66dc6b11c58051faae0b51
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-16 15:29:31 +0200

    test: add test for visibility of slider

--- tests/slider.spec.ts
+++ tests/slider.spec.ts
@@ -1,7 +1,10 @@
+import { SliderPage } from '../pages/slider.page'
 import { expect, test } from '@playwright/test'
 
 test.describe('Slider of Price Range', () => {
+  let sliderPage: SliderPage
   test.beforeEach(async ({ page }) => {
+    sliderPage = new SliderPage(page)
     await page.goto('/')
     if (
       (await page.title()) !== 'Practice Software Testing - Toolshop - v5.0'
@@ -9,24 +12,17 @@ test.describe('Slider of Price Range', () => {
       throw new Error('Page title is not as expected')
     }
   })
-  test('Verify that slider can be moved to maximum value 200 without errors', async ({
-    page,
-  }) => {
-    const slider = page.locator('.ngx-slider-selection-bar')
+  test('Verify that slider is visible', async () => {
+    const slider = sliderPage.slider
+    await sliderPage.isSliderVisible()
     await expect(slider).toBeVisible()
-    const sliderPoint = page.locator('.ngx-slider-pointer').nth(1)
-    await expect(sliderPoint).toBeVisible()
+  })
+  test('Verify that slider can be moved to maximum value 200 without errors', async () => {
+    const sliderPoint = sliderPage.sliderPoint
 
     await expect(sliderPoint).toHaveAttribute('aria-valuetext', '100')
 
-    async function setSliderValue(value: number): Promise<number> {
-      await sliderPoint.evaluate((el, value) => {
-        el.setAttribute('aria-valuenow', value.toString())
-        el.style.left = `${value}px`
-      }, value)
-      return value
-    }
-    await setSliderValue(200)
+    await sliderPage.setSliderValue(200)
     await expect(sliderPoint).toHaveAttribute('aria-valuenow', '200')
   })
 })
