a1a1b8f0a51dfc12e0e6fb741959ba4ffa685d12
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-16 15:29:53 +0200

    chore: add page object model for slider tests

---
+++ pages/slider.page.ts
@@ -0,0 +1,37 @@
+import { Locator, Page } from '@playwright/test'
+
+export class SliderPage {
+  readonly page: Page
+  readonly slider: Locator
+  readonly sliderPoint: Locator
+  constructor(page: Page) {
+    this.page = page
+
+    this.slider = this.page.locator('.ngx-slider-selection-bar')
+    this.sliderPoint = page.locator('.ngx-slider-pointer').nth(1)
+  }
+
+  async getSliderValue(): Promise<string> {
+    return await this.slider.getAttribute('aria-valuenow')
+  }
+
+  async setSliderValue(value: number): Promise<number> {
+    await this.sliderPoint.evaluate((el, value) => {
+      el.setAttribute('aria-valuenow', value.toString())
+      el.style.left = `${value}px`
+    }, value)
+    return value
+  }
+
+  async isSliderVisible(): Promise<boolean> {
+    return await this.slider.isVisible()
+  }
+
+  async getSliderMinValue() {
+    return await this.slider.getAttribute('aria-valuemin')
+  }
+
+  async getSliderMaxValue() {
+    return await this.slider.getAttribute('aria-valuemax')
+  }
+}
