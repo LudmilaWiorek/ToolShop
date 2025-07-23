0b7b296252a231e388b1afbb438d09769a1daa6a
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-23 15:23:31 +0200

    test: add test for ascending A-Z sort

--- tests/sort.spec.ts
+++ tests/sort.spec.ts
@@ -4,19 +4,42 @@ test.describe('Sort functionality', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
   })
-  test('Verify that sort options are visible', async ({ sortPage }) => {
-    const sortHeader = sortPage.sortHeader
-    const selectForm = sortPage.selectForm
+  test('Verify that sort options are visible', async ({ sortDropdownPage }) => {
+    const sortHeader = sortDropdownPage.sortHeader
+    const selectForm = sortDropdownPage.selectForm
     await expect(sortHeader).toBeVisible()
     await expect(sortHeader).toHaveText('Sort')
     await expect(selectForm).toBeVisible()
     await expect(selectForm).toHaveAttribute('aria-label')
   })
-  test('Verify that sort options are applied correctly', async ({
-    sortPage,
+  test('Verify that sort options are applied correctly - A-Z ascending order', async ({
+    sortDropdownPage,
     page,
   }) => {
-    const productName = sortPage.productNameHeaders
+    const productName = sortDropdownPage.productNameHeaders
+    const expectedTexts = [
+      ' Adjustable Wrench ',
+      ' Angled Spanner ',
+      ' Belt Sander ',
+      ' Bolt Cutters ',
+      ' Chisels Set ',
+      ' Circular Saw ',
+      ' Claw Hammer ',
+      ' Claw Hammer with Fiberglass Handle ',
+      ' Claw Hammer with Shock Reduction Grip ',
+    ]
+    await sortDropdownPage.clickSortOption()
+    await sortDropdownPage.openSortOptionsForAscendingOrder()
+    await page.waitForTimeout(1000) // Wait for the options to be applied
+    await expect(productName).toHaveCount(9)
+    const productNameText = await sortDropdownPage.getProductNames()
+    await expect(productNameText).toEqual(expectedTexts)
+  })
+  test('Verify that sort options are applied correctly - Z_A descending order', async ({
+    sortDropdownPage,
+    page,
+  }) => {
+    const productName = sortDropdownPage.productNameHeaders
     const expectedTexts = [
       ' Wood Saw ',
       ' Wood Carving Chisels ',
@@ -28,11 +51,11 @@ test.describe('Sort functionality', () => {
       ' Swiss Woodcarving Chisels ',
       ' Super-thin Protection Gloves ',
     ]
-    await sortPage.clickSortOption()
-    await sortPage.openSortOptions()
+    await sortDropdownPage.clickSortOption()
+    await sortDropdownPage.openSortOptionsForDescendingOrder()
     await page.waitForTimeout(1000) // Wait for the options to be applied
     await expect(productName).toHaveCount(9)
-    const productNameText = await sortPage.getProductNames()
+    const productNameText = await sortDropdownPage.getProductNames()
     await expect(productNameText).toEqual(expectedTexts)
   })
 })
