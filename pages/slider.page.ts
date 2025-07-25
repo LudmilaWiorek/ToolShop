8c0dcda9f07699e9171c6ef5f252685442edd9e1
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-25 14:17:04 +0200

    feat: add method for simulating mouse move

--- pages/slider.page.ts
+++ pages/slider.page.ts
@@ -61,4 +61,20 @@ export class SliderPage {
   async getSliderMaxValue(): Promise<string | null> {
     return await this.sliderPoint.getAttribute('aria-valuemax')
   }
+  async moveSliderWithMouse(
+    stepSize: number = 1,
+    steps: number = 10,
+    direction: 'left' | 'right',
+  ): Promise<void> {
+    const sliderTrack = await this.getBoundingBox()
+
+    const targetX = sliderTrack.x + sliderTrack.width / 2 // target x - srodek slidera
+    const targetY = sliderTrack.y + sliderTrack.height / 2
+    // add condition to determine the direction of movement
+    const offset = direction === 'left' ? -stepSize : -stepSize // offset - liczba pixeli o jakie chcemy przesunac
+    await this.page.mouse.move(targetX, targetY)
+    await this.page.mouse.down()
+    await this.page.mouse.move(targetX + offset, targetY, { steps }) // targetx + offset - daje nowa pozycje, do ktorej przesuwasz myszke
+    await this.page.mouse.up()
+  }
 }
