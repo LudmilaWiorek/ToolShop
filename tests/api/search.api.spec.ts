8462d75682eb84495c46f159d8d45f31ed7e1e37
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-27 15:37:28 +0200

    feat: add API search tests for existing and non-existing products

---
+++ tests/api/search.api.spec.ts
@@ -0,0 +1,38 @@
+import { ApiClass } from '../../pages/apiClass.page'
+import { expect, test } from '@playwright/test'
+
+test.describe('API Search Tests', () => {
+  let apiClass: ApiClass
+  test('API search for hammer (existing product) with return code 200', async ({
+    request,
+  }) => {
+    apiClass = new ApiClass(request)
+    const response = await request.get(`${apiClass.baseUrl}/products/search`, {
+      data: { q: 'hammer' },
+    })
+    expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+    expect(responseBody.total).toBeGreaterThan(0)
+
+    responseBody.data.forEach((product) => {
+      expect(product.name.toLowerCase()).toContain('hammer')
+    })
+  })
+
+  test('API search for non-existing product with return empty data', async ({
+    request,
+  }) => {
+    apiClass = new ApiClass(request)
+    const responseNotFound = await request.get(
+      `${apiClass.baseUrl}/products/search`,
+      {
+        data: { q: 'mouse' },
+      },
+    )
+    expect(responseNotFound.status()).toBe(200)
+
+    const responseBody = JSON.parse(await responseNotFound.text())
+    expect(responseBody.total).toBe(0)
+  })
+})
