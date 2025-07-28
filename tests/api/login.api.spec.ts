de30e4b3577d1ab3a205194227d4d062a5c58c25
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-28 11:32:10 +0200

    test: add beforeEach to describe (geting valid token), add test for successful login with the token and test for unsuccessful login withour token

--- tests/api/login.api.spec.ts
+++ tests/api/login.api.spec.ts
@@ -4,27 +4,44 @@ import { ApiClass } from '../../pages/apiClass.page'
 
 test.describe('testing login module', () => {
   let apiClass: ApiClass
-  //   let header: { Authorization: string }
+  let header: { Authorization: string }
   test.beforeEach(
-    'api login user with correct credentials',
+    'api login user with correct credentials', // we get a key to gate of the API :)
     async ({ request }) => {
       apiClass = new ApiClass(request)
+
       const response = await request.post(`${apiClass.baseUrl}/users/login`, {
         data: {
           email: users.userdata.user[0].email,
           password: users.userdata.user[0].password,
         },
       })
-      const responseBody = JSON.parse(await response.text())
-      //   console.log(responseBody)
+      const responseBody = await response.json()
 
       expect(responseBody).toHaveProperty('access_token')
       expect(responseBody.access_token).toBeTruthy()
       expect(response.status()).toBe(200)
 
-      let header = {
+      header = {
         Authorization: `Bearer ${responseBody.access_token}`,
       }
     },
   )
+  test('Successful login with valid token', async ({ request }) => {
+    // we use that key
+    const response = await request.get(`${apiClass.baseUrl}/users/me`, {
+      headers: header,
+    })
+
+    expect(response.status()).toBe(200)
+    const responseBody = await response.json()
+    expect(responseBody).toHaveProperty('email')
+    expect(responseBody.email).toBe(users.userdata.user[0].email)
+  })
+  test('Unsuccessful login - without token', async ({ request }) => {
+    // we use that key
+    const response = await request.get(`${apiClass.baseUrl}/users/me`)
+
+    expect(response.status()).toBe(401)
+  })
 })
