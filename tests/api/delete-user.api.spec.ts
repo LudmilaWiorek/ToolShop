f7d6c9787a3974736f8acc70509651f1bcc9bcef
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-30 21:18:36 +0200

    test: add two tests for admin role: delete user and delete product

--- tests/api/delete-user.api.spec.ts
+++ tests/api/delete-user.api.spec.ts
@@ -3,22 +3,38 @@ import {
   apiAdminFixtures as test,
 } from '../../fixtures/fixtures.fixture'
 
-test('delete user with valid token', async ({
-  apiClass,
-  apiHeader,
-  request,
-}) => {
-  const responseForAdmin = await request.get(`${apiClass.baseUrl}/users/me`, {
-    headers: apiHeader,
+test.describe('testing admin role', () => {
+  test.beforeEach('log admin', async ({ apiHeader, page }) => {
+    const adminToken = apiHeader.Authorization.replace('Bearer', '')
+
+    await page.goto('https://practicesoftwaretesting.com')
+
+    await page.evaluate((adminToken) => {
+      localStorage.setItem('auth-token', adminToken)
+    }, adminToken)
   })
-  expect(responseForAdmin.status()).toBe(200)
-  //at first get userId
-  const userId = '01K18EF8V47EW3BW51JS234DEB'
-  const response = await request.delete(`${apiClass.baseUrl}/users/${userId}`, {
-    headers: apiHeader,
+  test('delete user', async ({ page }) => {
+    await page.goto('/admin/users')
+    const deleteButton = page.locator('.btn-danger').nth(4)
+    await expect(deleteButton).toBeVisible()
+    await deleteButton.click()
+    const alertMessage = page.locator('#toast-container')
+    await expect(alertMessage).toHaveText(
+      ' Seems like this customer is used elsewhere.',
+    )
   })
 
-  expect(response.status()).toBe(204)
+  test('delete product', async ({ page }) => {
+    await page.goto('/admin/products')
 
-  await expect(response.text).toBe('')
+    const deleteButton = page.locator('.btn-danger').nth(0)
+    const productName = page.getByRole('cell', { name: 'Combination Pliers' })
+    const alertMessage = page.locator('#toast-container')
+
+    await deleteButton.click()
+    await expect.soft(productName).toBeVisible()
+    await expect(alertMessage).toHaveText(
+      'Seems like this product is used elsewhere.',
+    )
+  })
 })
