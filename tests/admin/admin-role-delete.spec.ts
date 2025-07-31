2a669a863f1b5e8e0df0584e63167a781e99c5c6
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-31 12:01:20 +0200

    chore: change file name for admin test describe

---
+++ tests/admin/admin-role-delete.spec.ts
@@ -0,0 +1,40 @@
+import {
+  expect,
+  apiAdminFixture as test,
+} from '../../fixtures/api-admin.fixture'
+
+test.describe('testing admin role', () => {
+  test.beforeEach('log admin', async ({ apiHeader, page }) => {
+    const adminToken = apiHeader.Authorization.replace('Bearer', '')
+
+    await page.goto('https://practicesoftwaretesting.com')
+
+    await page.evaluate((adminToken) => {
+      localStorage.setItem('auth-token', adminToken)
+    }, adminToken)
+  })
+  test('delete user', async ({ page }) => {
+    await page.goto('/admin/users')
+    const deleteButton = page.locator('.btn-danger').nth(2)
+    await expect(deleteButton).toBeVisible()
+    await deleteButton.click()
+    const alertMessage = page.locator('#toast-container')
+    await expect(alertMessage).toHaveText(
+      ' Seems like this customer is used elsewhere.',
+    )
+  })
+
+  test('delete product', async ({ page }) => {
+    await page.goto('/admin/products')
+
+    const deleteButton = page.locator('.btn-danger').nth(0)
+    const productName = page.getByRole('cell', { name: 'Combination Pliers' })
+    const alertMessage = page.locator('#toast-container')
+
+    await deleteButton.click()
+    await expect.soft(productName).toBeVisible()
+    await expect(alertMessage).toHaveText(
+      'Seems like this product is used elsewhere.',
+    )
+  })
+})
