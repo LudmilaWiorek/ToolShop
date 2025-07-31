2c3865ada0db0f02526aa7161c4f44d36233a9a4
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-07-31 12:04:59 +0200

    refactor login tests using api, implement new fixtures

--- tests/api/login.api.spec.ts
+++ tests/api/login.api.spec.ts
@@ -1,36 +1,15 @@
 import * as users from '../../JSONS/users.json'
-import { expect, fixtures as test } from '../../fixtures/fixtures.fixture'
-import { ApiClass } from '../../pages/apiClass.page'
+import { expect, apiUserFixture as test } from '../../fixtures/api-user.fixture'
 
 test.describe('testing login module', () => {
-  let apiClass: ApiClass
-  let header: { Authorization: string }
-  test.beforeEach(
-    'api login user with correct credentials', // we get a key to gate of the API :)
-    async ({ request }) => {
-      apiClass = new ApiClass(request)
-
-      const response = await request.post(`${apiClass.baseUrl}/users/login`, {
-        data: {
-          email: users.userdata.user[0].email,
-          password: users.userdata.user[0].password,
-        },
-      })
-      const responseBody = await response.json()
-
-      expect(responseBody).toHaveProperty('access_token')
-      expect(responseBody.access_token).toBeTruthy()
-      expect(response.status()).toBe(200)
-
-      header = {
-        Authorization: `Bearer ${responseBody.access_token}`,
-      }
-    },
-  )
-  test('Successful login with valid token', async ({ request }) => {
-    // we use that key
+  test('Successful login with valid token', async ({
+    request,
+    apiClass,
+    apiHeader,
+  }) => {
+    // we use that keyS
     const response = await request.get(`${apiClass.baseUrl}/users/me`, {
-      headers: header,
+      headers: apiHeader,
     })
 
     expect(response.status()).toBe(200)
@@ -38,17 +17,24 @@ test.describe('testing login module', () => {
     expect(responseBody).toHaveProperty('email')
     expect(responseBody.email).toBe(users.userdata.user[0].email)
   })
-  test('Testing endpoint users/me - without token', async ({ request }) => {
+
+  test('Testing endpoint users/me - without token', async ({
+    apiClass,
+    request,
+  }) => {
     const response = await request.get(`${apiClass.baseUrl}/users/me`)
 
     expect(response.status()).toBe(401)
     const responseBody = await response.json()
+    expect(responseBody).toHaveProperty('message')
     expect(responseBody.message).toBe('Unauthorized')
   })
+
   test('Testing endpoint users/me - with invalid token', async ({
+    apiClass,
     request,
   }) => {
-    header = {
+    const header = {
       Authorization: 'Bearer fakeToken',
     }
     const response = await request.get(`${apiClass.baseUrl}/users/me`, {
