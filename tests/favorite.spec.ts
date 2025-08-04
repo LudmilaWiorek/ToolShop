af28f6b768992620cd562a93123a237bf4e53da3
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-04 10:23:02 +0200

    Revert "test: rename describe and test titles for favorite"
    This reverts commit 81afc7f6af95334fa8925ac482d23f4005eb4994.

--- tests/favorite.spec.ts
+++ tests/favorite.spec.ts
@@ -1,6 +1,6 @@
 import { expect, fixtures as test } from '../fixtures/fixtures.fixture'
 
-test.describe('Testing favorite module', () => {
+test.describe('testing favorite module', () => {
   test.beforeEach('login with correct credentials', async ({ loginPage }) => {
     const arrayUrl = await loginPage.page.url().split('/')
     const lastArrayElement = await arrayUrl[arrayUrl.length - 1]
@@ -9,7 +9,7 @@ test.describe('Testing favorite module', () => {
     }
     await loginPage.goToPage()
   })
-  test('successful add to favorite section', async ({
+  test('positive - add to favorite section', async ({
     accessoryPage,
     favoritePage: favoritePage,
   }) => {
