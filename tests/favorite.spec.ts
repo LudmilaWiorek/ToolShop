bd8026db47860fe26a72cb6e88aeada3c0c11bc3
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-04 14:40:48 +0200

    fix(favorite): remove beforeEach and implement loginPAge fixture into test parameters

--- tests/favorite.spec.ts
+++ tests/favorite.spec.ts
@@ -1,18 +1,14 @@
 import { expect, fixtures as test } from '../fixtures/fixtures.fixture'
 
 test.describe('Testing favorite module', () => {
-  test.beforeEach('login with correct credentials', async ({ loginPage }) => {
-    const arrayUrl = await loginPage.page.url().split('/')
-    const lastArrayElement = await arrayUrl[arrayUrl.length - 1]
-    if (lastArrayElement !== 'account') {
-      throw 'login failed'
-    }
-    await loginPage.goToPage()
-  })
   test('successful add to favorite section', async ({
     accessoryPage,
     favoritePage: favoritePage,
+    loginPage,
   }) => {
+    loginPage
+    await expect(favoritePage.page).toHaveURL(/.*\/account/)
+
     const slipJointPliersName = 'Slip Joint Pliers'
     const itemAddedToFavorite = 'Product added to your favorites list.'
 
