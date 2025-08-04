4773fc923e2f6786ee33b8b23a131069c6d3d44c
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-04 11:45:23 +0200

    test: rename test describe and test for favorite

--- tests/favorite.spec.ts
+++ tests/favorite.spec.ts
@@ -1,6 +1,6 @@
 import { expect, fixtures as test } from '../fixtures/fixtures.fixture'
 
-test.describe('testing favorite module', () => {
+test.describe('Testing favorite module', () => {
   test.beforeEach('login with correct credentials', async ({ loginPage }) => {
     const arrayUrl = await loginPage.page.url().split('/')
     const lastArrayElement = await arrayUrl[arrayUrl.length - 1]
@@ -9,7 +9,7 @@ test.describe('testing favorite module', () => {
     }
     await loginPage.goToPage()
   })
-  test('positive - add to favorite section', async ({
+  test('successful add to favorite section', async ({
     accessoryPage,
     favoritePage: favoritePage,
   }) => {
