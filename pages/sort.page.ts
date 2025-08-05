5e94f218c0d51c7324bb1df2c336c814ce2c2134
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-05 13:15:22 +0200

    fix(sort): extend SortPage from BasePage

--- pages/sort.page.ts
+++ pages/sort.page.ts
@@ -1,20 +1,20 @@
 import { OrderByEnum } from '../enums/orderBy.enum'
+import { BasePage } from './base.page'
 import { Locator, Page } from '@playwright/test'
 
-export class SortDropdownPage {
-  readonly page: Page
+export class SortDropdownPage extends BasePage {
   readonly sortHeader: Locator
   readonly selectForm: Locator
   readonly productNameHeaders: Locator
 
   constructor(page: Page) {
-    this.page = page
+    super(page)
 
     this.sortHeader = this.page
       .locator('//h4[contains(@class, "grid-title")]')
       .nth(0)
-    this.selectForm = page.locator('.form-select')
-    this.productNameHeaders = page.locator('h5')
+    this.selectForm = this.page.locator('.form-select')
+    this.productNameHeaders = this.page.locator('h5')
   }
 
   async clickSortOption(): Promise<void> {
