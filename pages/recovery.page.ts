55d5e3fc0913dcba3c8b8dc1ecb8b2128875a95e
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-05 13:12:08 +0200

    fix(recovery): extend RecoveryPage from BasePage

--- pages/recovery.page.ts
+++ pages/recovery.page.ts
@@ -1,15 +1,15 @@
 import * as users from '../JSONS/users.json'
+import { BasePage } from './base.page'
 import { Locator, Page } from '@playwright/test'
 
-export class RecoveryPage {
-  readonly page: Page
+export class RecoveryPage extends BasePage {
   readonly forgetPasswordLink: Locator
   readonly emailInput: Locator
   readonly setNewPasswordButton: Locator
   readonly h3ForgotPassword: Locator
 
   constructor(page: Page) {
-    this.page = page
+    super(page)
     this.forgetPasswordLink = page.locator('.ForgetPwd')
     this.emailInput = page.locator('#email')
     this.setNewPasswordButton = page.locator('.btnSubmit')
