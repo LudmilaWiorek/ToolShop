c9d5060bce2ef41e961146ea4f049f653c2324df
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-09 19:45:09 +0200

    feat: add ProductModel interface for creating product by admin

---
+++ models/addProduct.model.ts
@@ -0,0 +1,11 @@
+export interface ProductModel {
+  name: string
+  description: string
+  stock: number
+  price: number
+  location_offer: boolean
+  item_for_rent: boolean
+  brand: string
+  category: string
+  image: string
+}
