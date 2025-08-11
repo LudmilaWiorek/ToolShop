1f465c26d54a4e784e47e3d47df0cd66d33b1a23
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-11 16:11:56 +0200

    test: add extra assert for get all brands test

--- tests/api/brands.api.spec.ts
+++ tests/api/brands.api.spec.ts
@@ -5,12 +5,22 @@ test.describe('Testing Brands API', () => {
     const response = await request.get(
       'https://api.practicesoftwaretesting.com/brands',
     )
+
     expect(response.status()).toBe(200)
 
-    // const responseBody = await response.json()
-    // console.log(responseBody)
+    const responseBody = await response.json()
+    // sprawdzamy nei tylko ze jest 200 tylko ze zwrocony obiekt ma trzy wymagane wartosci
+    expect(responseBody[0]).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: expect.any(String),
+        slug: expect.any(String),
+      }),
+    )
   })
-  test('should throw 404 code for non-existing brand', async ({ request }) => {
+  test('should throw 404 code for getting non-existing brand', async ({
+    request,
+  }) => {
     const response = await request.get(
       'https://api.practicesoftwaretesting.com/brands/dfghkgk',
     )
@@ -19,10 +29,16 @@ test.describe('Testing Brands API', () => {
   test('should throw 405 code when trying to create a brand', async ({
     request,
   }) => {
-    const response = await request.post(
+    const response = await request.get(
       'https://api.practicesoftwaretesting.com/brands?name=something&slug=somethingtoo',
     )
     // expect(response.status()).toBe(405)
     expect(response.status()).toBe(422)
   }) // ? user shouldn't be able to create product, along with Swagger there should be code 405, first it was  201, now 422...
+  test('Should successfully store new brand', async ({ request }) => {
+    const response = await request.post(
+      'https://api.practicesoftwaretesting.com/brands?name=Kraken&slug=Kulineczek',
+    )
+    expect(response.status()).toBe(201)
+  })
 })
