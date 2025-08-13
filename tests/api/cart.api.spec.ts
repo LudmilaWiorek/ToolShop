23779c0ccb350d0787dad13543be17d7a704d7fc
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-13 20:09:17 +0200

    test: add test of creating cart with not allowed method

--- tests/api/cart.api.spec.ts
+++ tests/api/cart.api.spec.ts
@@ -4,10 +4,21 @@ import {
 } from '../../fixtures/api-class.fixture.ts'
 
 test.describe('Testing api endpoints for cart', () => {
-  test('post cart', async ({ request, apiClass }) => {
+  test('successfully create cart', async ({ request, apiClass }) => {
     const response = await request.post(`${apiClass.baseUrl}/carts`)
     expect(response.status()).toBe(201)
     const responseBody = await response.json()
     expect(responseBody.id).toBeTruthy()
+  })
+  test('create cart using not allowed method', async ({
+    request,
+    apiClass,
+  }) => {
+    const response = await request.delete(`${apiClass.baseUrl}/carts`)
+    expect(response.status()).toBe(405)
+    const responseBody = await response.json()
+    await expect(responseBody.message).toBe(
+      'Method is not allowed for the requested route',
+    )
   })
 })
