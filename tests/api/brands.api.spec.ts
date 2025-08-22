0f713faa29e72cc3cca04af2ab1722e4cca2b391
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-08-22 15:36:02 +0200

    refactor test describe for api testing brand

--- tests/api/brands.api.spec.ts
+++ tests/api/brands.api.spec.ts
@@ -1,10 +1,11 @@
-import { expect, test } from '@playwright/test'
+import {
+  expect,
+  apiClassFixture as test,
+} from '../../fixtures/api-class.fixture.ts'
 
 test.describe('Testing Brands API', () => {
-  test('get all brands', async ({ request }) => {
-    const response = await request.get(
-      'https://api.practicesoftwaretesting.com/brands',
-    )
+  test('get all brands', async ({ request, apiClass }) => {
+    const response = await request.get(`${apiClass.baseUrl}/brands`)
 
     expect(response.status()).toBe(200)
 
@@ -20,41 +21,41 @@ test.describe('Testing Brands API', () => {
   })
   test('should throw 404 code for getting non-existing brand', async ({
     request,
+    apiClass,
   }) => {
-    const response = await request.get(
-      'https://api.practicesoftwaretesting.com/brands/dfghkgk',
-    )
+    const response = await request.get(`${apiClass.baseUrl}/brands/dfghkgk`)
     expect(response.status()).toBe(404)
   })
   test('should throw 405 code when trying to create a brand', async ({
     request,
+    apiClass,
   }) => {
     const response = await request.get(
-      'https://api.practicesoftwaretesting.com/brands?name=something&slug=somethingtoo',
+      `${apiClass.baseUrl}/brands?name=something&slug=somethingtoo`,
     )
     // expect(response.status()).toBe(405)
     expect(response.status()).toBe(200)
   }) // ? user shouldn't be able to create product, along with Swagger there should be code 405, first it was  201, now 422...
-  test('Should successfully store new brand', async ({ request }) => {
+  test('Should successfully store new brand', async ({ request, apiClass }) => {
     const response = await request.post(
-      'https://api.practicesoftwaretesting.com/brands?name=Kraken&slug=Kulineczek',
+      `${apiClass.baseUrl}/brands?name=Teelfon&slug=Kulin`,
     )
     expect(response.status()).toBe(422)
   })
   test('Should return 404 code when the resource is not found', async ({
     request,
+    apiClass,
   }) => {
     const response = await request.post(
-      'https://api.practicesoftwaretesting.com/brands?name=Ludka&slug="Herbatka',
+      `${apiClass.baseUrl}/brands?name=Ludka&slug="Herbatka`,
     )
     expect(response.status()).toBe(422) //? no code 404
   })
   test('should return code 200 for successful searching for brand', async ({
     request,
+    apiClass,
   }) => {
-    const response = await request.get(
-      `https://api.practicesoftwaretesting.com/brands/search`,
-    )
+    const response = await request.get(`${apiClass.baseUrl}/brands/search`)
     expect(response.status()).toBe(200)
     const body = await response.json()
     await expect(body[0].name).toBe('ForgeFlex Tools')
