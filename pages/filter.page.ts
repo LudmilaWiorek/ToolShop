4688962978b746dfe2d3df8e0e54bbb2a1129ebf
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-09-12 16:55:42 +0200

    feat: add FilterPage class for product filtering functionality

---
+++ pages/filter.page.ts
@@ -0,0 +1,39 @@
+import {
+  FilterBrand,
+  FilterCategory,
+  HandToolsFilter,
+  OtherFilter,
+  PowerToolsFilter,
+} from '../enums/filter.enum'
+import { BasePage } from './base.page'
+import { Locator, Page } from '@playwright/test'
+
+export class FilterPage extends BasePage {
+  readonly byBrandFilter: Locator
+  readonly forgeFlexCheckbox: Locator
+  readonly mightyCraftHardwareCheckbox: Locator
+
+  readonly noProductsFoundMessage: Locator
+
+  constructor(page: Page) {
+    super(page)
+
+    //filter by brand
+    this.byBrandFilter = page.getByText(' By Brand ')
+    this.forgeFlexCheckbox = page.getByText(' ForgeFlex ')
+    this.mightyCraftHardwareCheckbox = page.getByText(' MightyCraft Hardware ')
+
+    this.noProductsFoundMessage = page.getByText('There are no products found.')
+  }
+  async getLocatorByMainCategory(category: FilterCategory): Promise<Locator> {
+    return this.page.getByLabel(category)
+  }
+  async getLocatorBySubCategory(
+    subcategory: HandToolsFilter | PowerToolsFilter | OtherFilter,
+  ): Promise<Locator> {
+    return this.page.getByText(subcategory)
+  }
+  async getLocatorByBrand(brand: FilterBrand): Promise<Locator> {
+    return this.page.getByText(brand)
+  }
+}
