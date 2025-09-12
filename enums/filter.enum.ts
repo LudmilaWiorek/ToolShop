397ecb13ed65ce5958e6e8770ef8a50afbf1aaa9
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-09-12 16:12:33 +0200

    feat: add filter enums for checkbox of filters

---
+++ enums/filter.enum.ts
@@ -0,0 +1,34 @@
+export enum FilterCategory {
+  hand_tools = 'Hand Tools',
+  power_tools = 'Power Tools',
+  other = 'Other',
+}
+export enum FilterBrand {
+  forge_flex = 'ForgeFlex',
+  mighty_craft_hardware = 'MightyCraft Hardware',
+}
+export enum handToolsFilter {
+  handToolsFilter = ' Hand Tools ',
+  hammerCheckbox = ' Hammer ',
+  handSawCheckbox = ' Hand Saw ',
+  wrenchCheckbox = ' Wrench ',
+  screwDriverCheckbox = ' Screwdriver ',
+  pliersCheckbox = ' Pliers ',
+  chiselsCheckbox = ' Chisels ',
+  measuresCheckbox = ' Measuring Tools ',
+}
+export enum powerToolsFilter {
+  powerToolsFilter = ' Power Tools ',
+  grinderCheckbox = ' Grinder ',
+  sanderCheckbox = ' Sander ',
+  sawCheckbox = ' Saw ',
+  drillCheckbox = ' Drill ',
+}
+export enum otherFilter {
+  otherFilter = ' Other ',
+  toolBeltsCheckbox = ' Tool Belts ',
+  storagesolutionsCheckbox = ' Storage Solutions ',
+  workbenchCheckbox = ' Workbench ',
+  safetyGearCheckbox = ' Safety Gear ',
+  fastenersCheckbox = ' Fasteners ',
+}
